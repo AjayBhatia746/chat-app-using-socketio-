30cb09550152ace38573aed558b059bac829200f
AjayBhatia746 <bhatiaa746@gmail.com>
2019-05-16 00:09:45 +0530

    =Filtering location sharing acknowldegments

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,39 @@
-io()
+const $messageform=document.querySelector('#message-form')
+const $messageformInput=document.querySelector('input')
+const $messageformButton=document.querySelector('#button')
+const $locationbutton=document.querySelector('#location')
+const socket=io()
+socket.on('message',(message)=>{
+    console.log(message)
+})
+
+$messageform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+//disable form so that a person can't send any message till the previous message sent to the server for this code will be
+$messageformButton.setAttribute('disabled','disabled')
+
+
+    const message= e.target.elements.message.value
+    socket.emit('sendMessage',message,(error)=>{
+        //Here we are enabling the user to send the data
+        $messageformButton.removeAttribute('disabled')
+        if(error){
+           return  console.log(error)
+        }
+        console.log('Delivered')//whichever emits the callback function will send the callback function and those who recieve will run
+    })
+})
+$locationbutton.addEventListener('click',()=>{
+   //disable
+   $locationbutton.setAttribute('disabled','disabled')
+    if(!navigator.geolocation){
+       return alert('Geoloaction is not valid for your syste')
+   } 
+   navigator.geolocation.getCurrentPosition((position)=>{
+    $locationbutton.removeAttribute('disabled')
+    socket.emit('sendlocation',{latitude:position.coords.latitude,longitude:position.coords.longitude},()=>{
+        console.log('Location shared')
+    })
+   })
+
+})
--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@ const path=require('path')
 const express=require('express')
 const http=require('http')//http is a core module and we do n't need to install it
 const app=express()
+const Filter=require('bad-words')
 const socketio=require('socket.io')//it is used for duplex connection that server can send the response to client withou any request too.
 const server=http.createServer(app)//here it will create the server if we do n't write this thing than also express does it for use
 //but as the socketio librabary return a function and we need to pass a server thus wwe refactor it
 const io=socketio(server)
 const port=3000 || process.env.PORT
 const publicDirectoryPath=path.join(__dirname,'../public')
-app.use(express.static(publicDirectoryPath))
 
-io.on('connection',()=>{
-    console.log('This if from socketio')
+app.use(express.static(publicDirectoryPath))
+ 
+io.on('connection',(socket)=>{//socket is an object that contain information about new connection 
+    console.log('This is from socket.io')
+    //anything that I will provide after the countupdate will be use as callback we need a functon to catch it on index.js
+    
+    //io makes to emit the count to every single object connected socket.emit emit to a single connection only 
+   
+   socket.emit('message','Welcome')
+   socket.broadcast.emit('message','A new user have entered')// It is too show the message to everyone that a new user is added
+   socket.on('sendMessage',(message,callback)=>{
+    const filter=new Filter()
+    if(filter.isProfane(message)){
+       return callback('Profinity is not allowed')
+    }    
+    io.emit('message',message)
+        callback()
+   })
+   socket.on('sendlocation',(coords,callback)=>{
+        io.emit('message','https://www.google.com/maps/?q='+coords.latitude+','+coords.longitude )
+        callback()
+   })
+   socket.on('disconnect',()=>{
+       io.emit('message','A user have left')
+   })
 })
 
 
